2eee3130999d753a27eebb6f63f1cc227370b2e6
Ben Pedrick <bpedrick@gmail.com>
2022-04-03 11:57:02 -0500

    first draft read.me made

--- database.sql
+++ database.sql
@@ -8,3 +8,21 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+
+CREATE TABLE "user_preferences" (
+"preference_id" SERIAL PRIMARY KEY,
+"user_id" int REFERENCES "user" (id),
+"route_id" int NOT NULL,
+"route_label" VARCHAR(50),
+"direction_id" int NOT NULL,
+"direction_name" VARCHAR(20),
+"place_code" VARCHAR(4),
+"description" VARCHAR(50),
+"stop_id" int,
+"time" time,
+"phone" VARCHAR(11),
+"email" VARCHAR(50),
+"notify_mode" VARCHAR(6)
+);
